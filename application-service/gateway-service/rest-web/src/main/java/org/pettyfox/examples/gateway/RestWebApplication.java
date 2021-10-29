9f8b9a5f7147ba258b84216e27aab2c825154a3d
fengwei <mathcoder23@126.com>
2021-10-29 17:57:46 +0800

    update

--- application-service/gateway-service/rest-web/src/main/java/org/pettyfox/examples/gateway/RestWebApplication.java
+++ application-service/gateway-service/rest-web/src/main/java/org/pettyfox/examples/gateway/RestWebApplication.java
@@ -1,13 +1,10 @@
 package org.pettyfox.examples.gateway;
 
-import org.apache.dubbo.config.annotation.DubboReference;
-import org.pettyfox.examples.dubbo.GreetingsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,16 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableTransactionManagement
 @RestController
 public class RestWebApplication {
-    @DubboReference(version = "1.0.0", group = "test")
-    private GreetingsService demoService;
 
     public static void main(String[] args) {
         SpringApplication.run(RestWebApplication.class, args);
-    }
-
-    @GetMapping("dubbo")
-    public String get() {
-        return demoService.sayHi("aa");
     }
 
 }
--- application-service/gateway-service/rest-web/src/main/java/org/pettyfox/examples/gateway/rest/DictController.java
+++ application-service/gateway-service/rest-web/src/main/java/org/pettyfox/examples/gateway/rest/DictController.java
@@ -14,6 +14,8 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * 词典接口，将项目中使用的枚举类型转换为接口，统一供前端调用
+ *
  * @author Petty Fox
  * @version 1.0
  * @date 2021/4/16 13:27
@@ -21,15 +23,17 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/constant")
 public class DictController {
+    public static final String basePackage = "org.pettyfox.framework.service";
+
     @PostMapping("dict")
     public RestObjectResponse<Map<String, List<DictData>>> getDict() {
-        Map<String,List<DictData>> result = new HashMap<>(64);
-        ClassUtil.scanPackageBySuper("org.pettyfox.framework.service", BaseEnum.class).forEach(r -> {
+        Map<String, List<DictData>> result = new HashMap<>(64);
+        ClassUtil.scanPackageBySuper(basePackage, BaseEnum.class).forEach(r -> {
             List<DictData> dictDataList = new ArrayList<>();
 
-            String name = r.getName().substring(r.getName().lastIndexOf(".")+1).replace("$","_");
+            String name = r.getName().substring(r.getName().lastIndexOf(".") + 1).replace("$", "_");
             for (Object e : r.getEnumConstants()) {
-                if(e instanceof BaseEnum){
+                if (e instanceof BaseEnum) {
                     DictData dictData = new DictData();
                     BaseEnum ee = (BaseEnum) e;
                     dictData.setValue(String.valueOf(ee));
@@ -38,7 +42,7 @@ public class DictController {
                 }
             }
 
-            result.put(name,dictDataList);
+            result.put(name, dictDataList);
 
         });
 
